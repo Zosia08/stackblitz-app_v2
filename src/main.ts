8cffe5ad039a098b21f64341562cc13dbd3912b9
Zosia08 <zosiaazukowskaa@gmail.com>
2024-02-24 15:23:17 +0100

    Firtst commit

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
-import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import 'zone.js';
+import { Component } from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+import "zone.js";
+import { QuizQuestionComponent } from "./app/my-app/quiz/quiz-question/quiz-question.component";
+import { QuizComponent } from "./app/my-app/quiz/quiz.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
+  imports: [QuizComponent, QuizQuestionComponent],
+  templateUrl: "./main.html",
 })
 export class App {
-  name = 'Angular';
+  name = "Angular";
 }
 
 bootstrapApplication(App);
